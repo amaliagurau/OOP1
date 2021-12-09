4d92d9cb50251dd653374d5ef671b8f9f156002f
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2021-12-09 16:06:47 +0200

    oop1

---
+++ .vs/OOP1/ArrayHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class ArrayHelper
+    {
+        public static int[] Fibonacci(int n)
+
+        {
+            int[] f = new int[n+1];
+            f[0] = 0;
+
+            if (n == 0) return f;
+            f[1] = 1;
+
+            if (n == 1) return f;
+            int i = 1;
+
+
+            while (f[i] <= n)
+            {
+
+                i++;
+                f[i] = f[i - 1] + f[i - 2];
+
+            }
+
+            return f;
+
+        }
+
+    }
+}
+
